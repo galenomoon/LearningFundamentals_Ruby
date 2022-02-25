db6edf3127996c4312b1f0d71addb3d589ed4af8
galeno <gui.galenocover@gmail.com>
2022-02-25 12:17:43 -0300

    while and each

--- test_ruby.rb
+++ test_ruby.rb
@@ -99,10 +99,29 @@ num = gets.chomp.to_i
 result = (num % 2)
 result === 0 ? (puts "#{num} is even") : (puts "#{num} is odd")
 
+
+# -- While --
+
+puts "===== WHILE ====="
+i = 0
+print "Type a number: "
+num = gets.chomp.to_i
+
+while i <= num do 
+    puts "Count.. #{i}"
+    i += 1
+end
+
+# -- EACH --
+
+puts "===== EACH ====="
+print "Type a number: "
+num = gets.chomp.to_i
+
+(0...num).each do |i|
+    puts "Value: #{i}"
+end
 =end
-
-
-
 
 
 =begin 
