eebb343c5239b332b39abc1537709d9490f4073c
galeno <gui.galenocover@gmail.com>
2022-02-25 14:34:03 -0300

    Arrays and Vectors

---
+++ arrays.rb
@@ -0,0 +1,16 @@
+# -- ARRAYS --
+
+v = [1,8,2,9,0,5,3,6,4]
+
+# [LISTA].each CRIE |{VARIABLE}|
+v.each do |item|
+    puts "Number: #{item}"
+end
+
+v1 = []
+v1.push("Lua")
+v1.push("Galeno")
+
+v1.each do |item|
+    puts "Name: #{item}"
+end
